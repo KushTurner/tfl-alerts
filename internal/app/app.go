cea2b02148bdb9cf6e12c037df9d0f1c8fb3e0e4
KushTurner <107963246+KushTurner@users.noreply.github.com>
2024-12-14 15:53:44 +0000

    Setting up logger

---
+++ internal/app/app.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"context"
+	"log/slog"
+)
+
+type App struct {
+	logger *slog.Logger
+}
+
+func New(logger *slog.Logger) *App {
+	app := &App{
+		logger: logger,
+	}
+	return app
+}
+
+func (a *App) Start(ctx context.Context) error {
+	return nil
+}
