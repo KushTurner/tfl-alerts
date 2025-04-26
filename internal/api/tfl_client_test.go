8fa51cbd55712144254b1f29c7c37b482bb9e455
kusht <107963246+KushTurner@users.noreply.github.com>
2025-04-26 16:07:27 +0100

    Added unit test for getEnv

--- internal/api/tfl_client_test.go
+++ internal/api/tfl_client_test.go
@@ -12,42 +12,44 @@ func FakeTflClient(url string) *TflClient {
 }
 
 func TestGetAllDisruptions(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "GET", r.Method)
-		assert.Equal(t, "/Line/Mode/tube,overground,national-rail,elizabeth-line,dlr/Status", r.URL.Path)
-		assert.Equal(t, "", r.Header.Get("User-Agent"))
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(
-			`[
-			  {
-				"name": "Avanti West Coast",
-				"lineStatuses": [
+	t.Run("Can make request TFL to get all disruptions", func(t *testing.T) {
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "GET", r.Method)
+			assert.Equal(t, "/Line/Mode/tube,overground,national-rail,elizabeth-line,dlr/Status", r.URL.Path)
+			assert.Equal(t, "", r.Header.Get("User-Agent"))
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(
+				`[
 				  {
-					"statusSeverity": 9,
-					"statusSeverityDescription": "Minor Delays",
-					"reason": "https://www.nationalrail.co.uk/service-disruptions/polesworth-20241204/"
+					"name": "Avanti West Coast",
+					"lineStatuses": [
+					  {
+						"statusSeverity": 9,
+						"statusSeverityDescription": "Minor Delays",
+						"reason": "https://www.nationalrail.co.uk/service-disruptions/polesworth-20241204/"
+					  }
+					]
 				  }
-				]
-			  }
-			]`))
-	}))
-	defer s.Close()
+				]`))
+		}))
+		defer s.Close()
 
-	expectedTrainStatus := []TrainStatus{
-		{
-			Name: "Avanti West Coast",
-			LineStatuses: []LineStatus{
-				{
-					StatusSeverity:            9,
-					StatusSeverityDescription: "Minor Delays",
-					Reason:                    "https://www.nationalrail.co.uk/service-disruptions/polesworth-20241204/",
+		expectedTrainStatus := []TrainStatus{
+			{
+				Name: "Avanti West Coast",
+				LineStatuses: []LineStatus{
+					{
+						StatusSeverity:            9,
+						StatusSeverityDescription: "Minor Delays",
+						Reason:                    "https://www.nationalrail.co.uk/service-disruptions/polesworth-20241204/",
+					},
 				},
 			},
-		},
-	}
+		}
 
-	tfl := FakeTflClient(s.URL)
-	actualTrainStatus, _ := tfl.AllCurrentDisruptions()
+		tfl := FakeTflClient(s.URL)
+		actualTrainStatus, _ := tfl.AllCurrentDisruptions()
 
-	assert.Equal(t, expectedTrainStatus, actualTrainStatus)
+		assert.Equal(t, expectedTrainStatus, actualTrainStatus)
+	})
 }
