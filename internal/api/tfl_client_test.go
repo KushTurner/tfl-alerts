ef6860ab5882f785979ff4fe52bc28dd54fc6741
kusht <107963246+KushTurner@users.noreply.github.com>
2025-04-26 15:46:41 +0100

    Added better unit test for tfl client

--- internal/api/tfl_client_test.go
+++ internal/api/tfl_client_test.go
@@ -12,18 +12,42 @@ func FakeTflClient(url string) *TflClient {
 }
 
 func TestGetAllDisruptions(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		assert.Equal(t, "/Line/Mode/tube,overground,national-rail,elizabeth-line,dlr/Status", r.URL.Path)
+		assert.Equal(t, "", r.Header.Get("User-Agent"))
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(
-			`[{"name":"Avanti West Coast","lineStatuses":[{"statusSeverity":9,"statusSeverityDescription":"Minor Delays","reason":"https://www.nationalrail.co.uk/service-disruptions/polesworth-20241204/"}]}]`))
+		w.Write([]byte(
+			`[
+			  {
+				"name": "Avanti West Coast",
+				"lineStatuses": [
+				  {
+					"statusSeverity": 9,
+					"statusSeverityDescription": "Minor Delays",
+					"reason": "https://www.nationalrail.co.uk/service-disruptions/polesworth-20241204/"
+				  }
+				]
+			  }
+			]`))
 	}))
+	defer s.Close()
 
-	defer server.Close()
+	expectedTrainStatus := []TrainStatus{
+		{
+			Name: "Avanti West Coast",
+			LineStatuses: []LineStatus{
+				{
+					StatusSeverity:            9,
+					StatusSeverityDescription: "Minor Delays",
+					Reason:                    "https://www.nationalrail.co.uk/service-disruptions/polesworth-20241204/",
+				},
+			},
+		},
+	}
 
-	td := []TrainStatus{{Name: "Avanti West Coast", LineStatuses: []LineStatus{{StatusSeverity: 9, StatusSeverityDescription: "Minor Delays", Reason: "https://www.nationalrail.co.uk/service-disruptions/polesworth-20241204/"}}}}
+	tfl := FakeTflClient(s.URL)
+	actualTrainStatus, _ := tfl.AllCurrentDisruptions()
 
-	tfl := FakeTflClient(server.URL)
-	resp, _ := tfl.AllCurrentDisruptions()
-
-	assert.Equal(t, td, resp)
+	assert.Equal(t, expectedTrainStatus, actualTrainStatus)
 }
