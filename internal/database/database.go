b5cfc35b8a4f06d58b06d080d50b6227dfa17258
KushTurner <107963246+KushTurner@users.noreply.github.com>
2025-04-27 15:35:01 +0100

    Moving migration and seed to make it easier to test

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,9 +1,11 @@
 package database
 
 import (
+	"context"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
 )
 
 type DB struct {
@@ -15,7 +17,19 @@ type TflAlertsDatabase struct {
 	UsersRepository
 }
 
-func NewTflAlertsDatabase(db *DB) TflAlertsDatabase {
+func NewTflAlertsDatabase(ctx context.Context, config *Config) TflAlertsDatabase {
+	db, _ := Connect(ctx, config)
+
+	err := RunMigrate(config.ConnStr)
+	if err != nil {
+		log.Panicf("unable to run migration %v", err)
+	}
+
+	err = RunSeed(ctx, db.Pool)
+	if err != nil {
+		log.Panicf("unable to run seed %v", err)
+	}
+
 	return TflAlertsDatabase{
 		TrainsRepository: PostgresTrainsRepository{db},
 		UsersRepository:  PostgresUsersRepository{db},
